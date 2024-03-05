abb30ac0da0bb41846428de50af2117326af4e2e
cheyer <cheyer03@gmail.com>
2024-03-06 00:43:09 +0100

    increate coverage

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
-import { add } from "./math";
+import { add, subtract } from "./math";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test("subtracts 1 + 2 to equal 3", () => {
+  expect(subtract(6, 2)).toBe(4);
 });
